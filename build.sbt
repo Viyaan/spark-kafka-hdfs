1ee8403f4481d5e32dc91fc52981bf394c8c9f22
Viyaan <viyaan.86@gmail.com>
2017-10-09 01:12:23 +0530

    adding sql context.

--- build.sbt
+++ build.sbt
@@ -11,3 +11,6 @@ libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.2.0"
 libraryDependencies += "org.apache.spark" % "spark-streaming-kafka-0-10_2.11" % "2.2.0"
 
 libraryDependencies += "org.apache.kafka" % "kafka-clients" % "0.11.0.1"
+
+libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.2.0"
+
--- src/main/scala/com/spark/streaming/KafkaStreaming.scala
+++ src/main/scala/com/spark/streaming/KafkaStreaming.scala
@@ -10,12 +10,16 @@ import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.streaming.kafka010.KafkaUtils
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+
 
 object KafkaStreaming extends App {
 
   val conf = new org.apache.spark.SparkConf().setMaster("local[*]").setAppName("kafka-streaming")
-  val conext = new SparkContext(conf)
-  val ssc = new StreamingContext(conext, org.apache.spark.streaming.Seconds(10))
+  val context = new SparkContext(conf)
+  val ssc = new StreamingContext(context, org.apache.spark.streaming.Seconds(10))
   val kafkaParams = Map[String, Object](
     "bootstrap.servers" -> "localhost:9092",
     "key.deserializer" -> classOf[StringDeserializer],
@@ -30,12 +34,15 @@ object KafkaStreaming extends App {
     Subscribe[String, String](topics, kafkaParams))
 
   val content = stream.filter(x => x.value() != null)
+  val sqlContext = new org.apache.spark.sql.SQLContext(context)
+      import sqlContext.implicits._
 
   stream.map(_.value).foreachRDD(rdd => {
 
     rdd.foreach(println)
     if (!rdd.isEmpty()) {
-      rdd.saveAsTextFile("C:/data/spark/")
+      rdd.toDF("value").coalesce(1).write.mode(SaveMode.Append).json("C:/data/spark/")
+     // rdd.saveAsTextFile("C:/data/spark/")
     }
 
   })
